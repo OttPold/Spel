c66b999fa77ebce79f0ac5de7b5ce20b409c56da
Ott Pold <ott.pold@elev.ga.ntig.se>
2025-01-24 12:50:12 +0100

    fick en flick med rader

--- Tre i rad.py
+++ Tre i rad.py
@@ -1,6 +1,28 @@
+import pygame
+import sys
+
 matrix = [[" ", " "," "], 
           [" ", " "," "],
           [" ", " "," "]]
+
+
+__author__  = "Ott Rudolf Pöld"
+__version__ = "2.0.0"
+__email__   = "Ott.Pold@elev.ga.ntig.se"
+
+pygame.init()
+
+width, height = 300, 300
+line_color = (10, 10, 10)
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Tic-Tac-Toe")
+
+def draw_board():
+    screen.fill((255, 255, 255))
+    for row in range(1, 3):
+        pygame.draw.line(screen, line_color, (0, row * 100), (300, row * 100), 5)
+    for col in range(1, 3):
+        pygame.draw.line(screen, line_color, (col * 100, 0), (col * 100, 300), 5)
 
 
 def display_board():
@@ -31,27 +53,33 @@ def check_board():
 
     return None
 
+def main():
+    player_turn = 'X'
+    draw_board()
+    pygame.display.update()
 
-while True:
-    display_board()
-    result = check_board()
-    if result:
-        print(result)
-        break
+    while True:
+        display_board()
+        result = check_board()
+        if result:
+            print(result)
+            break
 
-    p1_row = int(input("Player 1 - Ange rad (0, 1, 2 ): "))
-    p1_col = int(input("Player 1 - Ange rad (0, 1, 2 ): "))
-    if matrix[p1_row][p1_col] == " ":
-        matrix[p1_row][p1_col] = "X"
+        p1_row = int(input("Player 1 - Ange rad (0, 1, 2 ): "))
+        p1_col = int(input("Player 1 - Ange rad (0, 1, 2 ): "))
+        if matrix[p1_row][p1_col] == " ":
+            matrix[p1_row][p1_col] = "X"
 
-    display_board()
-    result = check_board()
-    if result:
-        print(result)
-        break
+        display_board()
+        result = check_board()
+        if result:
+            print(result)
+            break
 
-    p2_row = int(input("Player 2 - Ange rad (0, 1, 2 ): "))
-    p2_col = int(input("Player 2 - Ange rad (0, 1, 2 ): "))
-    if matrix[p2_row][p2_col] == " ":
-        matrix[p2_row][p2_col] = "O"
+        p2_row = int(input("Player 2 - Ange rad (0, 1, 2 ): "))
+        p2_col = int(input("Player 2 - Ange rad (0, 1, 2 ): "))
+        if matrix[p2_row][p2_col] == " ":
+            matrix[p2_row][p2_col] = "O"
 
+if __name__ == "__main__":
+    main()
